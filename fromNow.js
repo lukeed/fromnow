ccb2de6609658db00ffb22576bf1264662a530ba
luke <luke.edwards05@gmail.com>
2015-11-13 09:43:08 -0800

    restructure & clean up code

--- fromNow.js
+++ fromNow.js
@@ -8,64 +8,61 @@
  * Copyright 2014, LUKEED
  * http://www.lukeed.com
  */
+// var defaults = {
+//   maxChunks: 10,
+//   useSuffix: false,
+//   useAnd: false
+// }
+
+var msMinute = 60 * 1000,
+  msHour = msMinute * 60,
+  msDay = msHour * 24,
+  msMonth = msDay * 30,
+  msYear = msDay * 365;
+
+function pluralize(val, str) {
+  var label = (val > 1) ? (str + 's') : str
+  return val ? (val + ' ' + label) : null
+}
+
 function fromNow(date, maxChunks, useAgo, useAnd) {
+  console.time( 'start' );
   // set default values if left undefined
-  maxChunks = typeof maxChunks !== 'undefined' ? maxChunks : 10;
-  useAgo = typeof useAgo !== 'undefined' ? useAgo : false;
-  useAnd = typeof useAnd !== 'undefined' ? useAnd : false;
+  maxChunks = maxChunks || 10;
+  useAgo = useAgo || false;
+  useAnd = useAnd || false;
 
   var milli = (new Date(date) - new Date()),
       ms = Math.abs(milli);
 
   var isPast = (useAgo && milli < 0) ? ' ago' : '';
 
-  var msMinute = 60 * 1000,
-      msHour = msMinute * 60,
-      msDay = msHour * 24,
-      msMonth = msDay * 30,
-      msYear = msDay * 365;
+  var timeframes = {
+    year: Math.floor(ms / msYear),
+    month: Math.floor((ms % msYear) / msMonth),
+    day: Math.floor((ms % msMonth) / msDay),
+    hour: Math.floor((ms % msDay) / msHour),
+    minute: Math.floor((ms % msHour) / msMinute)
+  };
 
-  var years = Math.floor(ms / msYear),
-      months = Math.floor((ms % msYear) / msMonth),
-      days = Math.floor((ms % msMonth) / msDay),
-      hours = Math.floor((ms % msDay) / msHour),
-      minutes = Math.floor((ms % msHour) / msMinute);
-
-  var yearsLabel = (years > 1) ? ' years' : ' year',
-      monthsLabel = (months > 1) ? ' months' : ' month',
-      daysLabel = (days > 1) ? ' days' : ' day',
-      hoursLabel = (hours > 1) ? ' hours' : ' hour',
-      minutesLabel = (minutes > 1) ? ' minutes' : ' minute';
-
-  var bundle = [
-      years ? years + yearsLabel : null,
-      months ? months + monthsLabel : null,
-      days ? days + daysLabel : null,
-      hours ? hours + hoursLabel : null,
-      minutes ? minutes + minutesLabel : null
-  ];
-
-  var compiled = [], limit = 0;
-  bundle.forEach(function(segment, index) {
-    // Limit the returned array to return 'maxChunks' of not-null segments
-    if (segment != null && (limit < maxChunks)) {
-      limit++;
-      compiled.push(segment);
-    }
-  });
-
-  var length = compiled.length;
-  compiled = compiled.join(', ') + isPast;
-
-  if (useAnd && length > 1) {
-    var split = compiled.split(', ');
-    if (length == 2) {
-      return split.join(' and ');
-    } else {
-      split[length - 1] = 'and ' + split[length - 1];
-      return split.join(', ');
-    }
-  } else {
-    return compiled;
+  var chunks = [], period;
+  for (period in timeframes) {
+    var val = timeframes[period];
+    chunks.push(pluralize(val, period));
   }
+
+  // Limit the returned array to return 'maxChunks' of non-null segments
+  var compiled = [], i, len = chunks.length;
+  for (i = 0; i < len; i++) {
+    if (chunks[i] && i < maxChunks) compiled.push(chunks[i]);
+  }
+
+  len = compiled.length;
+  console.timeEnd( 'start' );
+  if (useAnd && len > 1) {
+    if (len == 2) return compiled.join(' and ') + isPast;
+    compiled[len - 1] = 'and ' + compiled[len - 1];
+  }
+
+  return compiled.join(', ') + isPast;
 }
