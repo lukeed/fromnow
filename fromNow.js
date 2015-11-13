727aefb6dbf4b307e6513799b2f3ce74d88b04f8
luke <luke.edwards05@gmail.com>
2015-11-13 09:49:31 -0800

    AMD vs CommonJS vs window.global exposure

--- fromNow.js
+++ fromNow.js
@@ -14,55 +14,69 @@
 //   useAnd: false
 // }
 
-var msMinute = 60 * 1000,
-  msHour = msMinute * 60,
-  msDay = msHour * 24,
-  msMonth = msDay * 30,
-  msYear = msDay * 365;
+(function ( root, factory ) {
+  if ( typeof exports === 'object' ) {
+    // Common JS
+    module.exports = factory(root, root.document);
+  } else if ( typeof define === 'function' && define.amd ) {
+    // AMD
+    define(function() { return factory(root, root.document); });
+  } else {
+    // Browser global
+    root.fromNow = factory(root, root.document);
+  }
+}(typeof window !== 'undefined' ? window : this, function (window, document) {
+  'use strict';
 
-function pluralize(val, str) {
-  var label = (val > 1) ? (str + 's') : str
-  return val ? (val + ' ' + label) : null
-}
+  var msMinute = 60 * 1000,
+    msHour = msMinute * 60,
+    msDay = msHour * 24,
+    msMonth = msDay * 30,
+    msYear = msDay * 365;
 
-function fromNow(date, maxChunks, useAgo, useAnd) {
-  console.time( 'start' );
-  // set default values if left undefined
-  maxChunks = maxChunks || 10;
-  useAgo = useAgo || false;
-  useAnd = useAnd || false;
-
-  var milli = (new Date(date) - new Date()),
-      ms = Math.abs(milli);
-
-  var isPast = (useAgo && milli < 0) ? ' ago' : '';
-
-  var timeframes = {
-    year: Math.floor(ms / msYear),
-    month: Math.floor((ms % msYear) / msMonth),
-    day: Math.floor((ms % msMonth) / msDay),
-    hour: Math.floor((ms % msDay) / msHour),
-    minute: Math.floor((ms % msHour) / msMinute)
-  };
-
-  var chunks = [], period;
-  for (period in timeframes) {
-    var val = timeframes[period];
-    chunks.push(pluralize(val, period));
+  function pluralize(val, str) {
+    var label = (val > 1) ? (str + 's') : str
+    return val ? (val + ' ' + label) : null
   }
 
-  // Limit the returned array to return 'maxChunks' of non-null segments
-  var compiled = [], i, len = chunks.length;
-  for (i = 0; i < len; i++) {
-    if (chunks[i] && i < maxChunks) compiled.push(chunks[i]);
-  }
+  return function (date, maxChunks, useAgo, useAnd) {
+    // set default values if left undefined
+    maxChunks = maxChunks || 10;
+    useAgo = useAgo || false;
+    useAnd = useAnd || false;
 
-  len = compiled.length;
-  console.timeEnd( 'start' );
-  if (useAnd && len > 1) {
-    if (len == 2) return compiled.join(' and ') + isPast;
-    compiled[len - 1] = 'and ' + compiled[len - 1];
-  }
+    var milli = (new Date(date) - new Date()),
+        ms = Math.abs(milli);
 
-  return compiled.join(', ') + isPast;
-}
+    var isPast = (useAgo && milli < 0) ? ' ago' : '';
+
+    var timeframes = {
+      year: Math.floor(ms / msYear),
+      month: Math.floor((ms % msYear) / msMonth),
+      day: Math.floor((ms % msMonth) / msDay),
+      hour: Math.floor((ms % msDay) / msHour),
+      minute: Math.floor((ms % msHour) / msMinute)
+    };
+
+    var chunks = [], period;
+    for (period in timeframes) {
+      var val = timeframes[period];
+      chunks.push(pluralize(val, period));
+    }
+
+    // Limit the returned array to return 'maxChunks' of non-null segments
+    var compiled = [], i, len = chunks.length;
+    for (i = 0; i < len; i++) {
+      if (chunks[i] && i < maxChunks) compiled.push(chunks[i]);
+    }
+
+    len = compiled.length;
+
+    if (useAnd && len > 1) {
+      if (len == 2) return compiled.join(' and ') + isPast;
+      compiled[len - 1] = 'and ' + compiled[len - 1];
+    }
+
+    return compiled.join(', ') + isPast;
+  }
+}));
