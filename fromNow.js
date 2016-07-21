cb333c94ba61a25c09a67a78c789e546e82432cb
Luke Edwards <luke.edwards05@gmail.com>
2016-07-21 11:57:10 -0700

    condense module logic. spring cleaning

--- fromNow.js
+++ fromNow.js
@@ -1,26 +1,22 @@
 /**
- * FromNow
- * > Get readable time differences from now vs past or future dates.
- *
+ * FromNow - Get readable time differences from now vs past or future dates.
  * @author  Luke Edwards
  * @url     www.lukeed.com
  * @version 2.0
  */
-
-(function (root, factory) {
-  if (typeof exports === 'object') {
-    module.exports = factory(root, root.document); // Common JS
-  } else if ( typeof define === 'function' && define.amd ) {
-    define(function() { return factory(root, root.document); }); // AMD
+;(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory;
   } else {
-    root.fromNow = factory(root, root.document); // window global
+    root.fromNow = factory();
   }
-}(typeof window !== 'undefined' ? window : this, function (window, document) {
+}(this, function () {
   'use strict';
 
   function pluralize(val, str) {
-    var label = (val > 1) ? (str + 's') : str
-    return val ? (val + ' ' + label) : null
+    return val + ' ' + ((val > 1) ? (str + 's') : str);
   }
 
   var msMinute = 60 * 1000,
@@ -29,23 +25,13 @@
     msMonth = msDay * 30,
     msYear = msDay * 365;
 
-  var defaults = {
-    maxChunks: 10,
-    useAgo: false,
-    useAnd: false
-  }
-
   return function (date, opts) {
-    // set default values if left undefined
     opts = opts || {};
-    var maxChunks = opts.maxChunks || 10;
-    var useAgo = opts.useAgo || false;
-    var useAnd = opts.useAnd || false;
 
     var milli = (new Date(date) - new Date()),
         ms = Math.abs(milli);
 
-    var isPast = (useAgo && milli < 0) ? ' ago' : '';
+    if (ms === 0) return 'just now';
 
     var timeframes = {
       year: Math.floor(ms / msYear),
@@ -55,28 +41,29 @@
       minute: Math.floor((ms % msHour) / msMinute)
     };
 
-    var chunks = [], period;
+    var chunks = [], period, val;
     for (period in timeframes) {
-      var val = timeframes[period];
+      val = timeframes[period];
       chunks.push(pluralize(val, period));
     }
 
     // Limit the returned array to return 'maxChunks' of non-null segments
-    var compiled = [], i, limit = 0, len = chunks.length;
+    var compiled = [], i, len = chunks.length,
+      limit = 0, max = opts.maxChunks || 10;
     for (i = 0; i < len; i++) {
-      if (chunks[i] && limit < maxChunks) {
+      if (chunks[i] && limit < max) {
         limit++;
         compiled.push(chunks[i]);
       }
     }
 
-    len = compiled.length;
+    var sfx = (opts.useAgo && milli < 0) ? ' ago' : '';
 
-    if (useAnd && len > 1) {
-      if (len == 2) return compiled.join(' and ') + isPast;
-      compiled[len - 1] = 'and ' + compiled[len - 1];
+    if (opts.useAnd && limit > 1) {
+      if (limit === 2) return compiled.join(' and ') + sfx;
+      compiled[limit - 1] = 'and ' + compiled[limit - 1];
     }
 
-    return compiled.join(', ') + isPast;
+    return compiled.join(', ') + sfx;
   }
 }));
