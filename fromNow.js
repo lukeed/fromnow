9339d82f0c55d5a7efa04608e242eba0f9f5ad1a
Luke Edwards <luke.edwards05@gmail.com>
2016-11-10 15:10:11 -0800

    fix commonjs export

--- fromNow.js
+++ fromNow.js
@@ -7,7 +7,7 @@
 	if (typeof define === 'function' && define.amd) {
 		define(factory);
 	} else if (typeof exports === 'object') {
-		module.exports = factory;
+		module.exports = factory();
 	} else {
 		root.fromNow = factory();
 	}
