f527287e4cf96ab0efd32aac914d9ef0594be241
Luke Edwards <luke.edwards05@gmail.com>
2018-09-21 11:26:52 -0700

    refactor: use esmodule

--- src/index.js
+++ src/index.js
@@ -1,67 +1,49 @@
-/**
- * FromNow - Get readable time differences from now vs past or future dates.
- * @author  Luke Edwards (www.lukeed.com)
- * @version 2.1.1
- */
-(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		define(factory);
-	} else if (typeof exports === 'object') {
-		module.exports = factory();
-	} else {
-		root.fromNow = factory();
-	}
-}(this, function () {
-	'use strict';
+function pluralize(val, str) {
+	return val + ' ' + ((val > 1) ? (str + 's') : str);
+}
 
-	function pluralize(val, str) {
-		return val + ' ' + ((val > 1) ? (str + 's') : str);
-	}
+var MIN = 60 * 1e3;
+var HOUR = MIN * 60;
+var DAY = HOUR * 24;
+var YEAR = DAY * 365;
+var MONTH = DAY * 30;
 
-	var msMinute = 60 * 1000,
-		msHour = msMinute * 60,
-		msDay = msHour * 24,
-		msMonth = msDay * 30,
-		msYear = msDay * 365;
+export default function (date, opts) {
+	opts = opts || {};
+	date = new Date(date).getTime();
 
-	return function (date, opts) {
-		opts = opts || {};
+	var delta = date - Date.now();
+	if (delta < MIN) return 'just now';
 
-		var milli = (new Date(date) - new Date()),
-			ms = Math.abs(milli);
-
-		if (ms < msMinute) return 'just now';
-
-		var timeframes = {
-			year: Math.floor(ms / msYear),
-			month: Math.floor((ms % msYear) / msMonth),
-			day: Math.floor((ms % msMonth) / msDay),
-			hour: Math.floor((ms % msDay) / msHour),
-			minute: Math.floor((ms % msHour) / msMinute)
-		};
-
-		var chunks=[], period, val;
-		for (period in timeframes) {
-			val = timeframes[period];
-			chunks.push(pluralize(val, period));
-		}
-
-		// Limit the returned array to return 'max' of non-null segments
-		var i=0, limit=0, compiled=[], len=chunks.length, max=opts.max || 10;
-		for (; i < len; i++) {
-			if (chunks[i] && limit < max) {
-				limit++;
-				compiled.push(chunks[i]);
-			}
-		}
-
-		var sfx = (opts.ago && milli < 0) ? ' ago' : '';
-
-		if (opts.and && limit > 1) {
-			if (limit === 2) return compiled.join(' and ') + sfx;
-			compiled[limit - 1] = 'and ' + compiled[limit - 1];
-		}
-
-		return compiled.join(', ') + sfx;
+	var timeframes = {
+		year: Math.floor(ms / YEAR),
+		month: Math.floor((ms % YEAR) / MONTH),
+		day: Math.floor((ms % MONTH) / DAY),
+		hour: Math.floor((ms % DAY) / HOUR),
+		minute: Math.floor((ms % HOUR) / MIN)
 	};
-}));
+
+	var chunks=[], period, val;
+	for (period in timeframes) {
+		val = timeframes[period];
+		chunks.push(pluralize(val, period));
+	}
+
+	// Limit the returned array to return 'max' of non-null segments
+	var i=0, limit=0, compiled=[], len=chunks.length, max=opts.max || 10;
+	for (; i < len; i++) {
+		if (chunks[i] && limit < max) {
+			limit++;
+			compiled.push(chunks[i]);
+		}
+	}
+
+	var sfx = (opts.ago && delta < 0) ? ' ago' : '';
+
+	if (opts.and && limit > 1) {
+		if (limit === 2) return compiled.join(' and ') + sfx;
+		compiled[limit - 1] = 'and ' + compiled[limit - 1];
+	}
+
+	return compiled.join(', ') + sfx;
+}
